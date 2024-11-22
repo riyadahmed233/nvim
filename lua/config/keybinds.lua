a9579c37fd6270c2ad116b8fb163637bbaefb66a
Riyad Ahmed <mail@riyadahmed.net>
2024-11-22 11:01:11 +0000

    Kickstart nvim imported

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,91 +1,120 @@
 -- Keymap Helper Function
- function Map(mode, lhs, rhs, description, opts)
-     local options = { noremap = true, silent = true, desc = description}
-    if opts then
-         options = vim.tbl_extend("force", options, opts)
-     end
-     vim.keymap.set(mode, lhs, rhs, options)
- end
+function Map(mode, lhs, rhs, description, opts)
+  local options = { noremap = true, silent = true, desc = description }
+  if opts then
+    options = vim.tbl_extend('force', options, opts)
+  end
+  vim.keymap.set(mode, lhs, rhs, options)
+end
 
-vim.g.mapleader = " "
-
--- Window Movement
-Map("n", "<leader>wh", "<C-w>h", "Left window")
-Map("n", "<leader>wj", "<C-w>j", "Down Window")
-
-
-
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 -- Window Movement
-Map("n", "<leader>w", "", "Window")
+Map('n', '<leader>wh', '<C-w>h', 'Left window')
+Map('n', '<leader>wj', '<C-w>j', 'Down Window')
 
-Map("n", "<leader>wh", "<C-w>h", "Left window")
-Map("n", "<leader>wj", "<C-w>j", "Down Window")
-Map("n", "<leader>wk", "<C-w>k", "Above window")
-Map("n", "<leader>wl", "<C-w>l", "Right Window")
-Map("n", "<leader>ww", "<C-w>w", "Cycle Window")
-Map("n", "<leader>wq", ":q", "Quit Window")
+vim.g.mapleader = ' '
 
-Map("t", "<leader>wh", "<cmd>wincmd h<CR>", "Left window")
-Map("t", "<leader>wj", "<cmd>wincmd j<CR>", "Down Window")
-Map("t", "<leader>wk", "<cmd>wincmd k<CR>", "Above window")
-Map("t", "<leader>wl", "<cmd>wincmd l<CR>", "Right Window")
-Map("t", "<leader>ww", "<cmd>wincmd w<CR>", "Cycle Window")
-Map("n", "<leader>wq", "<cmd>q<CR>", "Quit Window")
+-- Window Movement
+Map('n', '<leader>w', '', 'Window')
+
+Map('n', '<C-h>', '<C-w>h', 'Left window')
+Map('n', '<C-j>', '<C-w>j', 'Down Window')
+Map('n', '<C-k>', '<C-w>k', 'Above window')
+Map('n', '<C-l>', '<C-w>l', 'Right Window')
+Map('n', '<leader>ww', '<C-w>w', 'Cycle Window')
+Map('n', '<leader>wq', ':q', 'Quit Window')
+
+Map('t', '<C-h>', '<cmd>wincmd h<CR>', 'Left window')
+Map('t', '<C-j>', '<cmd>wincmd j<CR>', 'Down Window')
+Map('t', '<C-k>', '<cmd>wincmd k<CR>', 'Above window')
+Map('t', '<C-l>', '<cmd>wincmd l<CR>', 'Right Window')
+Map('t', '<leader>ww', '<cmd>wincmd w<CR>', 'Cycle Window')
+Map('n', '<leader>wq', '<cmd>q<CR>', 'Quit Window')
 
 -- Window Resizing
-Map("n", "<leader>w<Up>", ":resize -2<CR>", "Window size down")
-Map("n", "<leader>w<Down>", ":resize +2<CR>", "Window size up")
-Map("n", "<leader>w<Left>", ":vertical resize +2<CR>", "Window size left")
-Map("n", "<leader>w<Right>", ":vertical resize -2<CR>", "Window size right")
+Map('n', '<leader>w<Up>', ':resize -2<CR>', 'Window size down')
+Map('n', '<leader>w<Down>', ':resize +2<CR>', 'Window size up')
+Map('n', '<leader>w<Left>', ':vertical resize +2<CR>', 'Window size left')
+Map('n', '<leader>w<Right>', ':vertical resize -2<CR>', 'Window size right')
 
-Map("t", "<leader>w<Up>", "<cmd>resize -2<CR>", "Window size down")
-Map("t", "<leader>w<Down>", "<cmd>resize +2<CR>", "Window size up")
-Map("t", "<leader>w<Left>", "<cmd>vertical resize +2<CR>", "Window size left")
-Map("t", "<leader>w<Right>", "<cmd>vertical resize -2<CR>", "Window size right")
+Map('t', '<leader>w<Up>', '<cmd>resize -2<CR>', 'Window size down')
+Map('t', '<leader>w<Down>', '<cmd>resize +2<CR>', 'Window size up')
+Map('t', '<leader>w<Left>', '<cmd>vertical resize +2<CR>', 'Window size left')
+Map('t', '<leader>w<Right>', '<cmd>vertical resize -2<CR>', 'Window size right')
 
 -- Window splitting
-Map("n", "<leader>w2", ":vsplit<CR><C-w>w", "Split Vertically")
-Map("n", "<leader>w3", ":hsplit<CR><C-w>w", "Split Horizontally")
+Map('n', '<leader>w2', ':vsplit<CR><C-w>w', 'Split Vertically')
+Map('n', '<leader>w3', ':split<CR><C-w>w', 'Split Horizontally')
 
-Map("n", "<leader>w2", "<cmd>vsplit<CR><C-w>w", "Split Vertically")
-Map("n", "<leader>w3", "<cmd>hsplit<CR><C-w>w", "Split Horizontally")
+Map('n', '<leader>w2', '<cmd>vsplit<CR><C-w>w', 'Split Vertically')
+Map('n', '<leader>w3', '<cmd>split<CR><C-w>w', 'Split Horizontally')
 
 -- Buffer switching
-Map("n", "<leader>b", "", "Buffer")
-Map("n", "<leader>bn", ":bn<CR>", "Next buffer")
-Map("n", "<leader>bp", ":bp<CR>", "Previous buffer")
-Map("n", "<leader>bk", ":bd<CR>", "Previous buffer")
+Map('n', '<leader>b', '', 'Buffer')
+Map('n', '<leader>bn', ':bn<CR>', 'Next buffer')
+Map('n', '<leader>bp', ':bp<CR>', 'Previous buffer')
+Map('n', '<leader>bk', ':bd<CR>', 'Previous buffer')
 
 -- Save
-Map("n", "<leader>bs", ":w<CR>", "Save buffer")
-Map("n", "<leader>s", ":w<CR>", "Save buffer")
+Map('n', '<leader>bs', ':w<CR>', 'Save buffer')
+Map('n', '<leader>s', ':w<CR>', 'Save buffer')
 
 -- Quit
-Map("n", "<leader>q", "", "Quit")
-Map("n", "<leader>qq", ":q<CR>", "Quit")
-Map("n", "<leader>qq", ":wq<CR>", "Write and Quit")
-Map("n", "<leader>qQ", ":q!<CR>", "Force Quit")
+Map('n', '<leader>q', '', 'Quit')
+Map('n', '<leader>qq', ':q<CR>', 'Quit')
+Map('n', '<leader>qq', ':wq<CR>', 'Write and Quit')
+Map('n', '<leader>qQ', ':q!<CR>', 'Force Quit')
 
 -- Undo-tree
-Map("n", "<leader>ut", ":UndotreeToggle<CR>:UndotreeFocus<CR>", "Quit")
+Map('n', '<leader>tu', ':UndotreeToggle<CR>:UndotreeFocus<CR>', 'Toggle Undotree')
+Map('n', '<leader>tn', ':Neotree toggle<CR>', 'Toggle Neotree')
 
 -- Telescope
 
-local builtin = require('telescope.builtin')
+local builtin = require 'telescope.builtin'
+
 -- Map("n", "<leader>ff", builtin.find_files, "Find Files")
-Map("n", "<leader>f", "", "Files")
-Map("n", "<leader>ff", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", "Find Files")
-Map("n", "<leader>fe", ":Telescope file_browser<CR>", "File Explorer")
-Map("n", "<leader>.", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", "Find Files")
-Map("n", "<leader>/", builtin.live_grep,"Live Grep")
-Map("n", "<leader>fa", "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>")
-Map("n", "<leader>fb", builtin.buffers, "Find Buffers")
-Map("n", "<leader>fg", builtin.live_grep, "Telescope Live Grep")
-Map("n", "<leader>fr", builtin.oldfiles,"Recent Files")
+Map('n', '<leader>f', '', 'Files')
+Map('n', '<leader>ff', ':Telescope file_browser path=%:p:h select_buffer=true<CR>', 'Find Files')
+Map('n', '<leader>fe', ':Telescope file_browser<CR>', 'File Explorer')
+Map('n', '<leader>.', ':Telescope file_browser path=%:p:h select_buffer=true<CR>', 'Find Files')
+-- Map("n", "<leader>/", builtin.live_grep,"Live Grep")
+Map('n', '<leader>fa', '<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>')
+Map('n', '<leader>fb', builtin.buffers, 'Find Buffers')
+Map('n', '<leader>fg', builtin.live_grep, 'Telescope Live Grep')
+Map('n', '<leader>fr', builtin.oldfiles, 'Recent Files')
 -- Map("n", "<leader>fc", "<cmd> Telescope colorschemes <CR>", "Colourschemes")
 
--- Config
-Map("n", "<leader>fc", ":Telescope file_browser path=~/.config/nvim select_buffer=true<CR>", "Open Config Directory")
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+  builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer' })
 
+-- Config
+Map('n', '<leader>fc', ':Telescope file_browser path=~/.config/nvim select_buffer=true<CR>', 'Open Config Directory')
+
+-- Kickstart.nvim
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
