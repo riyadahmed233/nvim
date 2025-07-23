d4872d8a031de8eac5b561cc5876e7ef9271f537
Riyad Ahmed <mail@riyadahmed.net>
2025-07-23 05:03:19 +0100

    updated

--- init.lua
+++ init.lua
@@ -759,6 +759,5 @@ require('lazy').setup({
 -- Import keybinds from file
 
 require 'config.keybinds'
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+++ lua/plugins/startup.lua
@@ -0,0 +1,6 @@
+return {
+  'eoh-bse/minintro.nvim',
+  opts = { color = '#828bb8' },
+  config = true,
+  lazy = false,
+}
